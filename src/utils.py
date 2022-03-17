67f61bc42d622b9da9f01ce2f3525cfda2717596
Eldos <eldos.baktybekov@gmail.com>
2022-03-17 22:23:48 +0600

    refactor(utils): remove log file

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,6 @@ __all__ = (
     'logger',
 )
 
-logger.add(config.LOG_FILE_PATH, retention='3 days')
-
 
 def read_accounts_file() -> list[dict]:
     with open(config.ACCOUNTS_FILE_PATH, encoding='utf-8') as accounts_file:
