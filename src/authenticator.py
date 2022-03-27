d39f9e4673706d2bae870ad5de996b63f5cf77e0
Eldos <eldos.baktybekov@gmail.com>
2022-03-27 20:34:07 +0600

    docs: add docs

--- src/authenticator.py
+++ src/authenticator.py
@@ -9,6 +9,12 @@ __all__ = (
 
 
 def update_account_cookies(account: DodoAccount, repeat_times: int = 5):
+    """Login in Dodo IS and save cookies in redis.
+
+    Args:
+        account: DodoAccount object.
+        repeat_times: if auth request was unsuccessful, the request will be sent again.
+    """
     try:
         cookies = account.get_auth_cookies()
         if not cookies:
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,13 @@
 import json
 
-from loguru import logger
-
 import config
-
-__all__ = (
-    'logger',
-)
 
 
 def read_accounts_file() -> list[dict]:
+    """Read accounts.json file in the root of repo.
+
+    Returns:
+        List of account objects.
+    """
     with open(config.ACCOUNTS_FILE_PATH, encoding='utf-8') as accounts_file:
         return json.load(accounts_file)
