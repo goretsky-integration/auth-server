154fe219900d8b68781af58fbc999e4d36a950e8
usbtypec1 <eldos.baktybekov@gmail.com>
2022-06-29 15:25:06 +0600

    :wrench: Add `MONGO_DB_URL` and `SECRET_KEY` fields in ``.env` file and `config.py`

--- src/config.py
+++ src/config.py
@@ -1,16 +1,14 @@
-import pathlib
-
 from environs import Env
 
 __all__ = (
     'COOKIES_LIFETIME',
     'COOKIES_UPDATE_THRESHOLD',
     'COOKIES_UPDATE_DELAY',
-    'SRC_DIR',
-    'ACCOUNTS_FILE_PATH',
     'REDIS_DB',
     'REDIS_HOST',
     'REDIS_PORT',
+    'SECRET_KEY',
+    'MONGO_DB_URL',
 )
 
 env = Env()
@@ -20,9 +18,9 @@ COOKIES_LIFETIME: int = env.int('COOKIES_LIVETIME')
 COOKIES_UPDATE_DELAY: int = env.int('COOKIES_UPDATE_DELAY')
 COOKIES_UPDATE_THRESHOLD: int = env.int('COOKIES_UPDATE_THRESHOLD')
 
-REDIS_DB = env.int('REDIS_DB')
-REDIS_HOST = env.str('REDIS_HOST')
-REDIS_PORT = env.int('REDIS_PORT')
+REDIS_DB: int = env.int('REDIS_DB')
+REDIS_HOST: str = env.str('REDIS_HOST')
+REDIS_PORT: int = env.int('REDIS_PORT')
 
-SRC_DIR = pathlib.Path(__file__).parent
-ACCOUNTS_FILE_PATH = pathlib.Path.joinpath(SRC_DIR.parent, 'accounts.json')
+SECRET_KEY: str = env.str('SECRET_KEY')
+MONGO_DB_URL: str = env.str('MONGO_DB_URL')
