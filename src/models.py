cbbf1b96bb57576f9fbd3666b52729918277289b
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-01 15:08:27 +0600

    :construction: Add model for token

--- src/models.py
+++ src/models.py
@@ -5,3 +5,11 @@ class Account(TypedDict):
     login: str
     name: str
     password: str
+
+
+class AuthCredentials(TypedDict):
+    access_token: str
+    expires_in: int
+    token_type: str
+    refresh_token: str
+    scope: str
