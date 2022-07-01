040dcd25432b1294b75d71d4006aa26ef7c0f5a5
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-01 15:10:49 +0600

    :wrench: Update `config.py` and `.env.dist`
    * Use REDIS_URL
    * Remove redundant env vars related to cookies
    * Add credentials for token auth
    * Add DEBUG mode

--- src/config.py
+++ src/config.py
@@ -2,25 +2,23 @@ from environs import Env
 
 __all__ = (
     'COOKIES_LIFETIME',
-    'COOKIES_UPDATE_THRESHOLD',
-    'COOKIES_UPDATE_DELAY',
-    'REDIS_DB',
-    'REDIS_HOST',
-    'REDIS_PORT',
+    'REDIS_URL',
     'SECRET_KEY',
     'MONGO_DB_URL',
+    'CLIENT_SECRET',
+    'CLIENT_ID',
+    'REDIRECT_URI',
+    'DEBUG',
 )
 
 env = Env()
 env.read_env()
 
-COOKIES_LIFETIME: int = env.int('COOKIES_LIVETIME')
-COOKIES_UPDATE_DELAY: int = env.int('COOKIES_UPDATE_DELAY')
-COOKIES_UPDATE_THRESHOLD: int = env.int('COOKIES_UPDATE_THRESHOLD')
-
-REDIS_DB: int = env.int('REDIS_DB')
-REDIS_HOST: str = env.str('REDIS_HOST')
-REDIS_PORT: int = env.int('REDIS_PORT')
-
+COOKIES_LIFETIME: int = env.int('COOKIES_LIFETIME')
 SECRET_KEY: str = env.str('SECRET_KEY')
 MONGO_DB_URL: str = env.str('MONGO_DB_URL')
+REDIS_URL: str = env.str('REDIS_URL')
+CLIENT_ID: str = env.str('CLIENT_ID')
+CLIENT_SECRET: str = env.str('CLIENT_SECRET')
+REDIRECT_URI: str = env.str('REDIRECT_URI')
+DEBUG: bool = env.bool('DEBUG')
