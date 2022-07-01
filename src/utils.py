eae46eb0707404682e33a46bfda512c30d4630bf
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-01 15:12:23 +0600

    :wrench: Add logging level to logger

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 import logging
 
+import config
+
 __all__ = (
     'logger',
 )
 
-logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+log_level = logging.DEBUG if config.DEBUG else logging.INFO
+
+logging.basicConfig(encoding='utf-8', level=log_level)
 logger = logging.getLogger()
