871a7f4d3880a1641b8f468f0862543c0c8e5c28
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-03 01:21:50 +0600

    :recycle: Re-setup tokens update entrypoint

--- src/update_tokens.py
+++ src/update_tokens.py
@@ -8,10 +8,9 @@ def main():
     accounts = db.get_accounts()
     office_manager_account_names = (account['name'] for account in accounts if account['name'].startswith('office'))
     for account_name in office_manager_account_names:
-        auth_credentials = db.get_token(account_name)
         try:
-            refresh_token = auth_credentials['refresh_token']
-        except KeyError:
+            refresh_token = db.local_storage.get_item(account_name)
+        except exceptions.LocalStorageItemIsNotFoundError:
             logger.error(f'Account {account_name} refresh token is missing')
             continue
         try:
@@ -19,7 +18,10 @@ def main():
         except exceptions.UnsuccessfulTokenRefreshError as error:
             logger.error(f'Could not update account {account_name} token:', str(error))
         else:
-            db.set_token(account_name, new_auth_credentials)
+            access_token = new_auth_credentials['access_token']
+            refresh_token = new_auth_credentials['refresh_token']
+            db.set_token(account_name, access_token)
+            db.local_storage.set_item(key=account_name, value=refresh_token)
             logger.info(f'Account {account_name} token has been updated')
 
 
