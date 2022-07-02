84c7d5c6c5ec00e78ecd23fb7f0c1843934c13d3
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-03 01:17:38 +0600

    :sparkles: Add local storage

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,2 +1,3 @@
 from .mongo_db import *
 from .redis_db import *
+from . import local_storage
--- src/exceptions.py
+++ src/exceptions.py
@@ -4,3 +4,10 @@ class UnsuccessfulAuthError(Exception):
 
 class UnsuccessfulTokenRefreshError(Exception):
     pass
+
+
+class LocalStorageItemIsNotFoundError(Exception):
+
+    def __init__(self, *args, key: str):
+        super().__init__(*args)
+        self.key = key
