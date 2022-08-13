412fe267da769dba4225d0d6fcc963fce713a51d
eldos <eldos.baktybekov@gmail.com>
2022-08-13 18:03:35 +0600

    :recycle: Use settings dependency

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 import logging
 
-import config
+from config import get_app_settings
 
 __all__ = (
     'logger',
 )
 
-log_level = logging.DEBUG if config.DEBUG else logging.INFO
+log_level = logging.DEBUG if get_app_settings().debug else logging.INFO
 
 logging.basicConfig(encoding='utf-8', level=log_level)
 logger = logging.getLogger()
