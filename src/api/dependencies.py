ff3cc88844dafa408bb34030ce75207ae27aaf8e
Eldos <eldos.baktybekov@gmail.com>
2023-02-17 17:28:00 +0600

    :sparkles: GI-10 Add account tokens endpoints

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,10 +1,17 @@
 from sqlalchemy.orm import sessionmaker
 
 from database.engine import engine
-from repositories import AccountRepository
+from repositories import AccountRepository, AccountTokensRepository
 
-__all__ = ('get_accounts_repository',)
+__all__ = (
+    'get_accounts_repository',
+    'get_account_tokens_repository',
+)
 
 
 def get_accounts_repository() -> AccountRepository:
     return AccountRepository(sessionmaker(engine))
+
+
+def get_account_tokens_repository() -> AccountTokensRepository:
+    return AccountTokensRepository(sessionmaker(engine))
--- src/api/routers/auth_credentials.py
+++ src/api/routers/auth_credentials.py
@@ -1,22 +1,30 @@
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Depends, status, Response
 
-from api import schemas
+from api import schemas, dependencies
+from repositories import AccountTokensRepository
 
 router = APIRouter(prefix='/auth', tags=['Auth credentials'])
 
 
-@router.get('/token/')
+@router.get('/token/', response_model=schemas.AccountTokens)
 def get_token(
         account_name: schemas.AccountName = Query(),
-) -> schemas.AccountTokens:
-    pass
+        accounts_tokens: AccountTokensRepository = Depends(dependencies.get_account_tokens_repository),
+):
+    return accounts_tokens.get_by_account_name(account_name)
 
 
 @router.patch('/token/')
 def update_token(
         tokens: schemas.AccountTokens = Body(),
+        accounts_tokens: AccountTokensRepository = Depends(dependencies.get_account_tokens_repository),
 ):
-    pass
+    accounts_tokens.update(
+        account_name=tokens.account_name,
+        access_token=tokens.access_token,
+        refresh_token=tokens.refresh_token,
+    )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.get('/cookies/')
