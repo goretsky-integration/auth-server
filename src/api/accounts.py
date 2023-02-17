5710c4646c3da1cc128a90e1e195a17f355f91dd
Eldos <eldos.baktybekov@gmail.com>
2023-02-17 14:17:35 +0600

    :sparkles: GI-10 Done accounts endpoint

--- src/api/accounts.py
+++ src/api/accounts.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Query
 
 from api import schemas
+from api.dependencies import get_accounts_repository
+from repositories import AccountRepository
 
 router = APIRouter(prefix='/accounts')
 
 
 @router.get('/')
-def get_all_accounts() -> list[schemas.Account]:
-    pass
+def get_all_accounts(
+        limit: int = Query(default=100),
+        skip: int = Query(default=0),
+        accounts: AccountRepository = Depends(get_accounts_repository),
+) -> list[schemas.Account]:
+    return accounts.get_all(limit=limit, skip=skip)
