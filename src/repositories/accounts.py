9f3f0d671214281f0681aa3aae88244b11c1d089
Eldos <eldos.baktybekov@gmail.com>
2023-02-17 13:48:54 +0600

    :sparkles: GI-10 Add accounts repository

---
+++ src/repositories/accounts.py
@@ -0,0 +1,27 @@
+from sqlalchemy import select
+
+import models
+from database.models import Account
+from repositories.base import Repository
+
+__all__ = ('AccountRepository',)
+
+
+class AccountRepository(Repository):
+
+    def get_all(self, *, limit: int, skip: int) -> list[models.Account]:
+        statement = (
+            select(Account)
+            .order_by(Account.name.asc())
+            .limit(limit)
+            .skip(skip)
+        )
+        with self._session_factory() as session:
+            accounts = session.scalars(statement).all()
+        return [
+            models.Account(
+                name=account.name,
+                login=account.login,
+                password=account.password,
+            ) for account in accounts
+        ]
