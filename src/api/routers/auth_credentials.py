68c35ef233ba46d8066fc10ab58db26a2e235352
Eldos <eldos.baktybekov@gmail.com>
2023-02-17 15:07:01 +0600

    :construction: GI-10 Declare auth endpoints schemas

--- src/api/routers/auth_credentials.py
+++ src/api/routers/auth_credentials.py
@@ -1,23 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Body
+
+from api import schemas
 
 router = APIRouter(prefix='/auth')
 
 
 @router.get('/token/')
-def get_token():
+def get_token(
+        account_name: schemas.AccountName = Query(),
+) -> schemas.AccountTokens:
     pass
 
 
 @router.patch('/token/')
-def update_token():
+def update_token(
+        tokens: schemas.AccountTokens = Body(),
+):
     pass
 
 
 @router.get('/cookies/')
-def get_cookies():
+def get_cookies(
+        account_name: schemas.AccountName = Query(),
+) -> schemas.AccountCookies:
     pass
 
 
 @router.patch('/cookies/')
-def update_cookies():
+def update_cookies(
+        cookies: schemas.AccountCookies = Body(),
+):
     pass
